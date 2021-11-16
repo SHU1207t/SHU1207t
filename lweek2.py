a40e734578f883568b98b632d37c8182c22f8f70
tsuchio <shu1207t@gmai.com>
2021-11-16 22:28:47 +0900

    test1116

--- lweek2.py
+++ lweek2.py
@@ -1,19 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 mat = np.loadtxt('heart.ldt', skiprows=1)
 que = np.array(mat[:, :-1])
-
 cmat = np.cov(que, rowvar=0, bias=0)
-
 e, v = np.linalg.eig(cmat)
-
 es = np.sort(e)[::-1]
-
 index = np.argsort(e)[::-1]
-
 mat0 = np.array([data[:-1] for data in mat if data[-1] == 0 ])
 mat1 = np.array([data[:-1] for data in mat if data[-1] == 1 ])
-
 
 vx0 = np.dot(v[index[0]],mat0.T)
 vy0 = np.dot(v[index[1]],mat0.T)
@@ -21,7 +16,4 @@ vx1 = np.dot(v[index[0]],mat1.T)
 vy1 = np.dot(v[index[1]],mat1.T)
 plt.scatter(vx0,vy0,c='b')
 plt.scatter(vx1,vy1,c='r')
-plt.savefig('plot.png')
-# print(es[1])
-# print(ey0)
-# print(v[1])
+plt.savefig('plot2.png')
